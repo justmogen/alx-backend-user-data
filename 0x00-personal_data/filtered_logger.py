08a5bc82994c51c65ec07c023ba31f9220c23b39
justmogen <justmogen@gmail.com>
2023-11-02 20:49:11 +0300

    regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,8 @@ import re  # regex
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ obfuscated logs """
     for field in fields:
         message = re.sub(field + "=.*?" + separator,
