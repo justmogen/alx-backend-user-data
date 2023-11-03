6653640e91dac2cbdf8b09189ad7c4c88dfd4f20
justmogen <justmogen@gmail.com>
2023-11-03 13:25:02 +0300

    hashed password and validation

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
-"""  """
+"""  password encryption """
 import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """  password encryption """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """  password encryption """
+    return bcrypt.checkpw(password.encode(), hashed_password)
