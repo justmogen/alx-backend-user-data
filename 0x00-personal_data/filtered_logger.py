06aa6127f6a5a24af71ac8d6b93741c80fee0cb4
justmogen <justmogen@gmail.com>
2023-11-03 13:02:16 +0300

    main()

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -62,3 +62,29 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         host=host,
         database=db_name)
     return connector
+
+
+def main():
+    """ obtain a database connection using get_db and retrieve
+     all rows in the users table
+    and display each row under a filtered format
+    """
+
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+
+    logger = get_logger()
+    for row in cursor:
+        msg = "name={}; email={}; phone={}; ssn={}; password={}; ip={};\
+                last_login={}; user_agent={}; ".format(
+            row[0], row[1], row[2], row[3], row[4], row[5],
+            row[6], row[7])
+        logger.info(msg)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
