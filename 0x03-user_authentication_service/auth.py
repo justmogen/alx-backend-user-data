f2e315a7e6221d3995698ebadbf762c192b42adf
justmogen <justmogen@gmail.com>
2023-11-16 12:10:27 +0300

    hashing, salting and encrypting password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Auth mmethod that takes in a password string arguments and 
+returns a byte string. 
+"""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """ Hashes a password. """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
