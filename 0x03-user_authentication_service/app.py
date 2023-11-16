b9e15622b3eadc3c31cad11e4665333bb528bbee
justmogen <justmogen@gmail.com>
2023-11-16 17:03:31 +0300

    sessions and manage an endpoint to register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 """ basic Flask app """
 from flask import Flask, jsonify
+from auth import Auth
+from typing import Union
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def root() -> str:
+def home() -> str:
     """ GET /
     Return:
       - welcome message
@@ -14,5 +17,17 @@ def root() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user() -> Union[str, tuple]:
+    """ end-point to register a user """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0, port=5000")
+    app.run(host="0.0.0.0, port=5000", debug=True)
